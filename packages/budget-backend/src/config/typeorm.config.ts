065d397c27da3e44fe2bd9e4201af1a28387bac6
ovieh <ovieh.mosley@gmail.com>
2019-10-20 16:06:31 +0100

    fixed obvious typo

--- packages/budget-backend/src/config/typeorm.config.ts
+++ packages/budget-backend/src/config/typeorm.config.ts
@@ -1,23 +1,23 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as config from 'config';
-// import * as PostGresConnectionStringParser from 'pg-connection-string';
+import * as PostGresConnectionStringParser from 'pg-connection-string';
 const dbConfig = config.get('db');
-// const connectionOptions = process.env.DATABASE_URL && PostGresConnectionStringParser.parse(process.env.DATABASE_URL);
-
-// export const typeOrmConfig: TypeOrmModuleOptions = {
-//   type: dbConfig.type,
-//   port: process.env.DATA_URL ? connectionOptions.port : dbConfig.port,
-//   host: process.env.DATA_URL ? connectionOptions.host : dbConfig.host,
-//   username: process.env.DATA_URL ? connectionOptions.user : dbConfig.username,
-//   password: process.env.DATA_URL ? connectionOptions.password : dbConfig.password,
-//   database: process.env.DATA_URL ? connectionOptions.database : dbConfig.database,
-//   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-//   synchronize: dbConfig.synchronize,
-// };
+const connectionOptions = process.env.DATABASE_URL && PostGresConnectionStringParser.parse(process.env.DATABASE_URL);
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: dbConfig.type,
-  url: process.env.DATABASE_URL,
+  port: process.env.DATABASE_URL ? connectionOptions.port : dbConfig.port,
+  host: process.env.DATABASE_URL ? connectionOptions.host : dbConfig.host,
+  username: process.env.DATABASE_URL ? connectionOptions.user : dbConfig.username,
+  password: process.env.DATABASE_URL ? connectionOptions.password : dbConfig.password,
+  database: process.env.DATABASE_URL ? connectionOptions.database : dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: dbConfig.synchronize,
 };
+
+// export const typeOrmConfig: TypeOrmModuleOptions = {
+//   type: dbConfig.type,
+//   url: process.env.DATABASE_URL,
+//   entities: [__dirname + '/../**/*.entity.{js,ts}'],
+//   synchronize: dbConfig.synchronize,
+// };
