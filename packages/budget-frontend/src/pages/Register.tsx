f14b376dabf96a702bd0a3c95b537e6f78c8e014
Ovieh Mosley <ovieh@intellisense.io>
2019-12-29 18:08:09 +0000

    switched register page to use formik

--- packages/budget-frontend/src/pages/Register.tsx
+++ packages/budget-frontend/src/pages/Register.tsx
@@ -1,44 +1,42 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useRegisterMutation } from '../generated/graphql';
 import { RouteComponentProps } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
 
 export const Register: React.FC<RouteComponentProps> = ({ history }) => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
     const [register] = useRegisterMutation();
 
     return (
-        <form
-            onSubmit={async e => {
-                e.preventDefault();
-                const response = await register({
+        <Formik
+            initialValues={{
+                password: '',
+                username: '',
+            }}
+            onSubmit={async (values, { setSubmitting }) => {
+                const { username, password } = values;
+                await register({
                     variables: {
                         username,
-                        password
-                    }
+                        password,
+                    },
                 });
+                setSubmitting(false);
                 history.push('/');
-                console.log(response);
             }}
         >
-            <div>
-                <input
-                    value={username}
-                    placeholder='username'
-                    onChange={e => {
-                        setUsername(e.target.value);
-                    }}
-                />
-                <input
-                    value={password}
-                    placeholder='password'
-                    type='password'
-                    onChange={e => {
-                        setPassword(e.target.value);
-                    }}
-                />
-            </div>
-            <button type='submit'>Register</button>
-        </form>
+            {({ handleSubmit, isSubmitting }) => (
+                <Form onSubmit={handleSubmit}>
+                    <Field name='username' placeholder='username' />
+                    <Field
+                        type='password'
+                        name='password'
+                        placeholder='password'
+                    />
+                    <button type='submit' disabled={isSubmitting}>
+                        Submit
+                    </button>
+                </Form>
+            )}
+        </Formik>
     );
 };
