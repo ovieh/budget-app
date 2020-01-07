34184bf9ffd44bfec563b84b83cc38ce57db6def
Ovieh Mosley <ovieh@intellisense.io>
2020-01-07 17:34:49 +0000

    beginnings of a styled signin screen

--- packages/budget-frontend/src/pages/SignIn.tsx
+++ packages/budget-frontend/src/pages/SignIn.tsx
@@ -1,53 +1,89 @@
-import React, { useState, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { useSignInMutation } from '../generated/graphql';
 import { RouteComponentProps } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
 import { setAccessToken } from '../accessToken';
 import { LoggedOutNav } from '../components/LoggedOutNav';
+import { Paper, Button, TextField } from '@material-ui/core';
+import styled from '@emotion/styled/macro';
 
 interface Props {}
 
+const PaperStyled = styled(Paper)`
+    margin-top: 10px;
+    max-width: 40rem;
+    height: 40rem;
+`;
+
+const FieldStyled = styled(Field)`
+    /* width: 100%; */
+    padding: 1rem 2rem;
+    box-sizing: border-box;
+`;
+
+const FormStyled = styled(Form)`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
+
+const ButtonStyled = styled(Button)`
+    width: 8rem;
+    height: 2.5rem;
+`;
+
 export const SignIn: React.FC<RouteComponentProps> = ({ history }) => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
     const [signIn] = useSignInMutation();
     return (
         <Fragment>
             <LoggedOutNav />
-            <form
-                onSubmit={async e => {
-                    e.preventDefault();
-                    const response = await signIn({
-                        variables: {
-                            username,
-                            password,
-                        },
-                    });
-
-                    history.push('/');
-                    if (response && response.data) {
-                        setAccessToken(response.data.signIn.accessToken);
-                    }
-                }}
-            >
-                <div>
-                    <input
-                        value={username}
-                        placeholder='username'
-                        onChange={e => {
-                            setUsername(e.target.value);
-                        }}
-                    />
-                    <input
-                        value={password}
-                        placeholder='password'
-                        type='password'
-                        onChange={e => {
-                            setPassword(e.target.value);
-                        }}
-                    />
-                </div>
-                <button type='submit'>Sign In</button>
-            </form>
+            <PaperStyled elevation={5}>
+                <Formik
+                    initialValues={{
+                        password: '',
+                        username: '',
+                    }}
+                    onSubmit={async (values, { setSubmitting }) => {
+                        const { username, password } = values;
+                        const response = await signIn({
+                            variables: {
+                                username,
+                                password,
+                            },
+                        });
+                        history.push('/');
+                        response &&
+                            response.data &&
+                            setAccessToken(response.data.signIn.accessToken);
+                    }}
+                >
+                    {({ handleSubmit, isSubmitting }) => (
+                        <FormStyled onSubmit={handleSubmit}>
+                            <FieldStyled
+                                as={TextField}
+                                name='username'
+                                placeholder='username'
+                                variant='outlined'
+                            />
+                            <FieldStyled
+                                as={TextField}
+                                name='password'
+                                placeholder='password'
+                                type='password'
+                                variant='outlined'
+                            />
+                            <ButtonStyled
+                                variant='contained'
+                                type='submit'
+                                disabled={isSubmitting}
+                                color='primary'
+                            >
+                                Sign In
+                            </ButtonStyled>
+                        </FormStyled>
+                    )}
+                </Formik>
+            </PaperStyled>
         </Fragment>
     );
 };
