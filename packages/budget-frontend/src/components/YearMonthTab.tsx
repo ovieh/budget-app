ab2eee3b005dea131784dbd57dcc822002eb4b3b
Ovieh Mosley <omokoro@gmail.com>
2020-01-11 13:38:00 +0000

    updated transaction entry form

--- packages/budget-frontend/src/components/YearMonthTab.tsx
+++ packages/budget-frontend/src/components/YearMonthTab.tsx
@@ -2,27 +2,21 @@ import React, { useState } from 'react';
 import { useGetYearMonthQuery, YearMonth } from '../generated/graphql';
 import { Tab, Tabs } from '@material-ui/core';
 import styled from '@emotion/styled/macro';
-import { useApolloClient } from '@apollo/react-hooks';
 
 // TODO fix horrible typings
 interface Props {
-    active: number;
-    setActive: any;
-    setYearMonth: any;
+    setYearMonth?: (arg0: any) => any;
 }
 
-export const YearMonthTab: React.FC<Props> = ({
-    active,
-    setActive,
-    setYearMonth,
-}) => {
+export const YearMonthTab: React.FC<Props> = ({ setYearMonth }) => {
     const { data } = useGetYearMonthQuery();
 
+    const [active, setActive] = useState(0);
 
     const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
         event.preventDefault();
         setActive(newValue);
-        setYearMonth(data?.getYearMonth[newValue]);
+        setYearMonth!(data?.getYearMonth[newValue]);
     };
 
     return (
@@ -31,6 +25,7 @@ export const YearMonthTab: React.FC<Props> = ({
             onChange={handleChange}
             indicatorColor='primary'
             textColor='primary'
+            scrollButtons='auto'
         >
             {data &&
                 data.getYearMonth.map((el, i) => (
--- packages/budget-frontend/src/pages/Transactions.tsx
+++ packages/budget-frontend/src/pages/Transactions.tsx
@@ -2,33 +2,56 @@ import React, { Fragment, useState } from 'react';
 import {
     useCreateTransactionMutation,
     useTransactionByMonthYearQuery,
-    YearMonth,
+    TransactionByMonthYearDocument,
+    useGetYearMonthQuery,
 } from '../generated/graphql';
 import styled from '@emotion/styled/macro';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { ReusuableTable } from '../components/ReusableTable';
 import { LoggedInNav } from '../components/LoggedInNav';
-import { Container, Grid, Paper } from '@material-ui/core';
+import {
+    Container,
+    Grid,
+    Paper,
+    TextField,
+    Typography,
+    Select,
+    MenuItem,
+    InputLabel,
+    FormControl,
+    Button,
+} from '@material-ui/core';
 import { YearMonthTab } from '../components/YearMonthTab';
 
 interface Props {}
 
+export interface IYearMonth {
+    year: number;
+    month: number;
+}
+
+const Label = styled(Typography)`
+    padding-top: 20px;
+`;
+
 export const Transactions: React.FC<Props> = () => {
-    const [active, setActive] = useState(0);
-    const [yearMonth, setYearMonth] = useState<YearMonth>({
-        month: 1,
-        year: 2020,
+    const { data: date } = useGetYearMonthQuery();
+
+    const [yearMonth, setYearMonth] = useState<IYearMonth | undefined>({
+        month: date?.getYearMonth[0].month || 2,
+        year: date?.getYearMonth[0].year || 2020,
     });
+
+    // TODO fix the above
+    console.log(date?.getYearMonth[0].month);
 
     const { data, error, loading } = useTransactionByMonthYearQuery({
-        errorPolicy: 'all',
+        skip: !date,
         variables: {
-            month: yearMonth.month,
-            year: yearMonth.year,
+            month: yearMonth!.month,
+            year: yearMonth!.year,
         },
     });
-
-
 
     const [addTransaction] = useCreateTransactionMutation();
 
@@ -53,9 +76,10 @@ export const Transactions: React.FC<Props> = () => {
     }
 
     // TODO handle loading better
-    if (loading) {
-        return <div>Loading Transactions</div>;
-    }
+    // if (loading) {
+    //     return <div>Loading Transactions</div>;
+    // }
+
     return (
         <Fragment>
             <LoggedInNav />
@@ -66,12 +90,8 @@ export const Transactions: React.FC<Props> = () => {
                 style={{ marginTop: '20px' }}
             >
                 <Grid item xs={7}>
-                    <Paper style={{ height: '40rem' }}>
-                        <YearMonthTab
-                            active={active}
-                            setActive={setActive}
-                            setYearMonth={setYearMonth}
-                        />
+                    <Paper>
+                        <YearMonthTab setYearMonth={setYearMonth} />
                         {data && (
                             <ReusuableTable
                                 columns={TransactionsColumns}
@@ -81,7 +101,179 @@ export const Transactions: React.FC<Props> = () => {
                     </Paper>
                 </Grid>
                 <Grid item xs={4}>
-                    <Paper style={{ height: '40rem' }}></Paper>
+                    <Paper style={{ minHeight: '20rem' }}>
+                        <Formik
+                            initialValues={{
+                                transactionDate: '',
+                                transactionType: '',
+                                sortCode: '',
+                                transactionDescription: '',
+                                accountNumber: '',
+                                debitAmount: '',
+                                creditAmount: '',
+                                balance: '',
+                            }}
+                            onSubmit={async (values, { setSubmitting }) => {
+                                await addTransaction({
+                                    variables: {
+                                        transactionDate: values.transactionDate,
+                                        transactionType: values.transactionType,
+                                        sortCode: values.sortCode,
+                                        transactionDescription:
+                                            values.transactionDescription,
+                                        accountNumber: values.accountNumber,
+                                        debitAmount: parseFloat(
+                                            values.debitAmount
+                                        ),
+                                        creditAmount: parseFloat(
+                                            values.creditAmount
+                                        ),
+                                        balance: parseFloat(values.balance),
+                                    },
+                                    refetchQueries: [
+                                        {
+                                            query: TransactionByMonthYearDocument,
+                                            variables: {
+                                                month: yearMonth!.month,
+                                                year: yearMonth!.year,
+                                            },
+                                        },
+                                    ],
+                                });
+                                setSubmitting(false);
+                            }}
+                        >
+                            {({ handleSubmit, isSubmitting }) => (
+                                <Form>
+                                    <Container>
+                                        <Label variant='h6' align='left'>
+                                            Enter Transaction
+                                        </Label>
+                                        <Grid container spacing={2}>
+                                            <Grid item xs={12}>
+                                                <Field
+                                                    item
+                                                    name='transactionDate'
+                                                    placeholder='Transaction Date'
+                                                    as={TextField}
+                                                    fullWidth
+                                                    required
+                                                    variant='outlined'
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <Field
+                                                    item
+                                                    name='transactionDescription'
+                                                    placeholder='Transaction Description'
+                                                    as={TextField}
+                                                    fullWidth
+                                                    required
+                                                    variant='outlined'
+                                                />
+                                            </Grid>
+                                            <Grid item xs={6}>
+                                                <FormControl required>
+                                                    <InputLabel id='type'>
+                                                        Type
+                                                    </InputLabel>
+                                                    <Field
+                                                        item
+                                                        name='transactionType'
+                                                        // placeholder='Transaction Type'
+                                                        as={Select}
+                                                        required
+                                                        variant='outlined'
+                                                        labelId='type'
+                                                    >
+                                                        <MenuItem value={'DEB'}>
+                                                            DEB
+                                                        </MenuItem>
+                                                        <MenuItem value={'DD'}>
+                                                            DD
+                                                        </MenuItem>
+                                                        <MenuItem value={'CPT'}>
+                                                            CPT
+                                                        </MenuItem>
+                                                        <MenuItem value={'FPO'}>
+                                                            FPO
+                                                        </MenuItem>
+                                                    </Field>
+                                                </FormControl>
+                                            </Grid>
+                                            <Grid item xs={6}>
+                                                <Field
+                                                    item
+                                                    name='sortCode'
+                                                    placeholder='Sort Code'
+                                                    as={TextField}
+                                                    fullWidth
+                                                    required
+                                                    variant='outlined'
+                                                />
+                                            </Grid>
+                                            <Grid item xs={6}>
+                                                <Field
+                                                    item
+                                                    name='accountNumber'
+                                                    placeholder='accountNumber'
+                                                    as={TextField}
+                                                    fullWidth
+                                                    required
+                                                    variant='outlined'
+                                                />
+                                            </Grid>
+                                            <Grid item xs={6}>
+                                                <Field
+                                                    item
+                                                    name='debitAmount'
+                                                    placeholder='Debit Amount'
+                                                    as={TextField}
+                                                    fullWidth
+                                                    required
+                                                    variant='outlined'
+                                                />
+                                            </Grid>
+                                            <Grid item xs={6}>
+                                                <Field
+                                                    item
+                                                    name='creditAmount'
+                                                    placeholder='Credit Amount'
+                                                    as={TextField}
+                                                    fullWidth
+                                                    required
+                                                    variant='outlined'
+                                                />
+                                            </Grid>
+                                            <Grid item xs={6}>
+                                                <Field
+                                                    item
+                                                    name='balance'
+                                                    placeholder='balance'
+                                                    as={TextField}
+                                                    fullWidth
+                                                    required
+                                                    variant='outlined'
+                                                />
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <Button
+                                                    type='submit'
+                                                    disabled={isSubmitting}
+                                                    variant='contained'
+                                                    centerRipple
+                                                    color='primary'
+                                                    fullWidth
+                                                >
+                                                    Submit
+                                                </Button>
+                                            </Grid>
+                                        </Grid>
+                                    </Container>
+                                </Form>
+                            )}
+                        </Formik>
+                    </Paper>
                 </Grid>
             </Grid>
             {/* <Wrapper>
