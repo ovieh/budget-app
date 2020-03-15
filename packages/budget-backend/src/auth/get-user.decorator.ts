b6a020fc5753dc101606416d649d02ba62cdbc65
Ovieh Mosley <omokoro@gmail.com>
2020-03-15 11:55:02 +0000

    updated user decorators

--- packages/budget-backend/src/auth/get-user.decorator.ts
+++ packages/budget-backend/src/auth/get-user.decorator.ts
@@ -1,15 +1,22 @@
-import { createParamDecorator } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Response } from 'express';
 import { User } from './user.entity';
+import { GqlExecutionContext } from '@nestjs/graphql';
 
-export const GetUser = createParamDecorator((data, req): User => {
-  return req.user;
-});
+export const GetUser = createParamDecorator(
+  (data, req): User => {
+    return req.user;
+  },
+);
 
 export const CurrentUser = createParamDecorator(
-  (data, [root, args, ctx, info]) => ctx.req.user,
+  (data: unknown, ctx: ExecutionContext): User => {
+    return GqlExecutionContext.create(ctx).getContext().req.user;
+  },
 );
 
 export const ResGql = createParamDecorator(
-  (data, [root, args, ctx, info]): Response => ctx.res,
+  (data: unknown, ctx: ExecutionContext): Response => {
+    return GqlExecutionContext.create(ctx).getContext().res;
+  },
 );
