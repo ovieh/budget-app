1b481979f9bdb1628849f50ea6d5476236a0a325
Ovieh Mosley <ovieh@intellisense.io>
2020-04-06 09:01:26 +0100

    cleanup

--- packages/budget-backend/src/fileupload/fileupload.controller.ts
+++ packages/budget-backend/src/fileupload/fileupload.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Logger, Post, UseInterceptors, UploadedFile, UseGuards } from '@nestjs/common';
+import { Controller, Post, UseInterceptors, UploadedFile, UseGuards } from '@nestjs/common';
 import { FileInterceptor} from '@nestjs/platform-express';
 import { FileuploadService } from './fileupload.service';
 import { User } from '../auth/user.entity';
-import { CurrentUser, GetUser } from '../auth/get-user.decorator';
+import {  GetUser } from '../auth/get-user.decorator';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('fileupload')
@@ -19,7 +19,6 @@ export class FileuploadController {
     @UploadedFile() file: Buffer,
     @GetUser() user: User,
   ): Promise<string> {
-    // console.log(`===================GET UUSER ${user}`)
     try {
       await this.fileuploadService.importFile(file, user);
 
