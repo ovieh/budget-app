2187f591e345f65f1d7080d76396a039f33fb630
Ovieh Mosley <ovieh@intellisense.io>
2020-12-01 22:23:56 +0000

    refactor: replaced formik with RHF on transaction form

--- packages/budget-frontend/src/pages/Transactions/Components/TransactionForm/TransactionForm.tsx
+++ packages/budget-frontend/src/pages/Transactions/Components/TransactionForm/TransactionForm.tsx
@@ -2,26 +2,23 @@ import React, { useContext } from 'react';
 import {
     Container,
     Paper,
-    // FormControl,
-    // InputLabel,
     Typography,
     TextField,
     Grid,
-    // MenuItem,
     Button,
     createStyles,
     makeStyles,
     Theme,
-    // Select,
 } from '@material-ui/core';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { FileUpload } from '../../../../components/FileUpload';
+import { useForm } from 'react-hook-form';
 import {
     useCreateTransactionMutation,
-    YearMonth,
     TransactionsByMonthAndYearDocument,
 } from '../../../../generated/graphql';
 import { ActiveDateContext, updateActiveDate } from '../../../../Contexts/ActiveDate';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -30,6 +27,17 @@ const useStyles = makeStyles((theme: Theme) =>
         },
     })
 );
+
+const TransactionSchema = yup.object().shape({
+    date: yup.date().required('Date is required'),
+    description: yup.string().min(2, 'Too Short!').max(20, 'Too long!').required('Requiried'),
+    debitAmount: yup.number().positive('Must be positive').required('Must be a number'),
+    balance: yup.number().positive('Must be positive').required('Must be a number'),
+    type: yup.string().default(''),
+    sortCode: yup.string().default('0'),
+    creditAmount: yup.string().default('0'),
+    accountNumber: yup.string().default('0'),
+});
 
 export const TransactionForm: React.FC<{}> = () => {
     const classes = useStyles();
@@ -43,6 +51,12 @@ export const TransactionForm: React.FC<{}> = () => {
         { name: 'balance', placeholder: 'Balance', type: 'text' },
     ];
 
+    const { register, errors, handleSubmit, formState } = useForm({
+        resolver: yupResolver(TransactionSchema),
+    });
+
+    const { isSubmitting } = formState;
+
     // TODO: Figure out better way of dealing with dates!
     const updateContextDate = (date: string) => {
         const month = date.split('-')[1];
@@ -53,6 +67,48 @@ export const TransactionForm: React.FC<{}> = () => {
         });
     };
 
+    // initialValues={{
+    //     date: '',
+    //     type: 'none',
+    //     sortCode: '0',
+    //     description: '',
+    //     accountNumber: '0',
+    //     debitAmount: '',
+    //     creditAmount: '0',
+    //     balance: '',
+    // }}
+
+    const onSubmit = async (values: any) => {
+        const date = new Date(values.date);
+        // adding 1 is cRazY !!!
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        const newTransaction = await addTransaction({
+            variables: {
+                date: values.date,
+                type: values.type,
+                sortCode: values.sortCode,
+                description: values.description,
+                accountNumber: values.accountNumber,
+                debitAmount: parseFloat(values.debitAmount),
+                creditAmount: parseFloat(values.creditAmount),
+                balance: parseFloat(values.balance),
+            },
+            awaitRefetchQueries: true,
+            refetchQueries: [
+                {
+                    query: TransactionsByMonthAndYearDocument,
+                    variables: {
+                        month,
+                        year,
+                    },
+                },
+            ],
+        });
+        newTransaction.data && updateContextDate(newTransaction.data.createTransaction.date);
+        // setSubmitting(false);
+    };
+
     return (
         <Paper style={{ minHeight: '20rem' }}>
             {/* <Grid container> */}
@@ -60,94 +116,49 @@ export const TransactionForm: React.FC<{}> = () => {
                 <FileUpload />
             </Container>
             {/* </Grid> */}
-            <Formik
-                initialValues={{
-                    date: '',
-                    type: 'none',
-                    sortCode: '0',
-                    description: '',
-                    accountNumber: '0',
-                    debitAmount: '',
-                    creditAmount: '0',
-                    balance: '',
-                }}
-                onSubmit={async (values, { setSubmitting }) => {
-                    const date = new Date(values.date);
-                    // adding 1 is cRazY !!!
-                    const month = date.getMonth() + 1;
-                    const year = date.getFullYear();
-
-                    const newTransaction = await addTransaction({
-                        variables: {
-                            date: values.date,
-                            type: values.type,
-                            sortCode: values.sortCode,
-                            description: values.description,
-                            accountNumber: values.accountNumber,
-                            debitAmount: parseFloat(values.debitAmount),
-                            creditAmount: parseFloat(values.creditAmount),
-                            balance: parseFloat(values.balance),
-                        },
-                        awaitRefetchQueries: true,
-                        refetchQueries: [
-                            {
-                                query: TransactionsByMonthAndYearDocument,
-                                variables: {
-                                    month,
-                                    year,
-                                },
-                            },
-                        ],
-                    });
-                    newTransaction.data &&
-                        updateContextDate(newTransaction.data.createTransaction.date);
-                    setSubmitting(false);
-                }}
-            >
-                {({ handleSubmit, isSubmitting }) => (
-                    <Form onSubmit={handleSubmit}>
-                        <Container>
-                            <Typography variant='h6' align='left' className={classes.label}>
-                                Create New Category
-                            </Typography>
-                            <Grid container spacing={2}>
-                                {FormFields.map(({ name, placeholder, type }) => (
-                                    <Grid item xs={12} key={name}>
-                                        <Field
-                                            name={name}
-                                            placeholder={placeholder}
-                                            fullWidth
-                                            required
-                                            variant='outlined'
-                                            size='small'
-                                            label={placeholder}
-                                            as={TextField}
-                                            type={type}
-                                            InputLabelProps={{
-                                                shrink: true,
-                                            }}
-                                        />
-                                    </Grid>
-                                ))}
-
-                                <Grid item xs={12}>
-                                    <Button
-                                        type='submit'
-                                        disabled={isSubmitting}
-                                        variant='contained'
-                                        centerRipple
-                                        color='primary'
-                                        fullWidth
-                                        size='small'
-                                    >
-                                        Submit
-                                    </Button>
-                                </Grid>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Container>
+                    <Typography variant='h6' align='left' className={classes.label}>
+                        Create New Category
+                    </Typography>
+                    <Grid container spacing={2}>
+                        {FormFields.map(({ name, placeholder, type }) => (
+                            <Grid item xs={12} key={name}>
+                                <TextField
+                                    name={name}
+                                    placeholder={placeholder}
+                                    fullWidth
+                                    required
+                                    variant='outlined'
+                                    size='small'
+                                    label={placeholder}
+                                    type={type}
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                    inputRef={register()}
+                                    error={!!errors[name]}
+                                    helperText={errors[name]?.message}
+                                />
                             </Grid>
-                        </Container>
-                    </Form>
-                )}
-            </Formik>
+                        ))}
+
+                        <Grid item xs={12}>
+                            <Button
+                                type='submit'
+                                disabled={isSubmitting}
+                                variant='contained'
+                                centerRipple
+                                color='primary'
+                                fullWidth
+                                size='small'
+                            >
+                                Submit
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </form>
         </Paper>
     );
 };
