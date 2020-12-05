1e30e230623a98241c6e2c69e2e0a351e2dd3f4f
Ovieh Mosley <ovieh@intellisense.io>
2020-12-05 18:05:52 +0000

    fix: add private route

--- packages/budget-frontend/src/Routes.tsx
+++ packages/budget-frontend/src/Routes.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Register } from './pages/Register';
 import { SignIn } from './pages/SignIn';
-import { Home } from './pages/Home';
 import { Categories } from './pages/Categories/Categories';
 import { Transactions } from './pages/Transactions/Transactions';
 import ProvideAuth, { PrivateRoute } from './Contexts/ProvideAuth';
@@ -12,7 +11,9 @@ export const Routes: React.FC = () => {
         <ProvideAuth>
             <BrowserRouter>
                 <Switch>
-                    <Route exact path='/' component={Transactions} />
+                    <PrivateRoute path='/transactions'>
+                        <Route exact path='/' component={Transactions} />
+                    </PrivateRoute>
                     <Route exact path='/register' component={Register} />
                     <Route exact path='/signin' component={SignIn} />
                     <PrivateRoute path='/transactions'>
